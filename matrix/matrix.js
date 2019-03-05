17220f11fd52c4c21e90ae8024c5e35be7de938d
seomis <seomis@riseup.net>
2019-03-05 22:53:12 +0000

    matrix improved

--- matrix/matrix.js
+++ matrix/matrix.js
@@ -1,40 +1,10 @@
-const rows = (sentence) =>  sentence
-		.split('\n')
-		.map( c=>{
-			return createRow(c)
-		});
-
-const createRow = (r) => r
-	.split(' ')
-	.map(number=>{
-		return parseInt(number);
-	});
-
-const columns = (rows) =>{
-        let columnsArr = [];
-        let tmpArr = [];
-        for (let j = 0; j < rows.length; j++){
-            for (let i = 0; i < rows[j].length; i++){
-				tmpArr.push(
-					parseInt(rows[i][j])
-				);
-            }
-            columnsArr.push(tmpArr);
-			if (j >= rows[j].length){
-				for (let i = 0; i < rows[j].length; i++){
-					columnsArr[i].push(
-						parseInt(rows[j][i])
-					);
-				}
-			}
-            tmpArr = [];
-        }
-        return columnsArr;
-}
-
-export class Matrix{
-	constructor(sentence){
-		this.rows = rows(sentence);
-		this.columns = columns(this.rows);
-	}
+export class Matrix {
+    constructor(str) {
+        this.rows = str.split('\n').map( r => r.split(' ').map( e => e * 1));
+        const columns = [];
+        const len = this.rows[0].length;
+        for (let i = 0; i < len; i++, columns.push([])){}
+        this.rows.forEach( (r) => r.forEach( (e, j) => columns[j].push(e)));
+        this.columns = columns;
+    }
 }
